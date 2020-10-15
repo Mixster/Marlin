d97c505513b1fe3e57a23c6fd55a581cb2753714
Mixster <66573535+Mixster@users.noreply.github.com>
2020-10-15 14:51:06 -0400

    version

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,8 +70,8 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Who made the changes.
-//#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
+#define STRING_CONFIG_H_AUTHOR "(Mike Mick, Ender-3 Pro, SKR V1.4 Turbo)" // Who made the changes.
+#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
  * *** VENDORS PLEASE READ ***
